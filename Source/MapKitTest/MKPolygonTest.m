3e916002f722d992155631a544be53020e88c142
Markus Schneider <markus.schneider@jemm.de>
2012-10-18 19:54:50 +0200

    Updated test case for MKPolygon.

--- Source/MapKitTest/MKPolygonTest.m
+++ Source/MapKitTest/MKPolygonTest.m
@@ -7,7 +7,7 @@
 //
 
 #import "MKPolygonTest.h"
-#import "MKPolygon.h"
+#import <MapKit/MapKit.h>
 
 #import <OCMock/OCMock.h>
 
@@ -34,6 +34,20 @@
 
 - (void)testPolygonWithCoordinates
 {
-    MKPolygon* polygon = [MKPolygon polygonWithCoordinates:<#(CLLocationCoordinate2D *)#> count:<#(NSUInteger)#>]
+    CLLocationCoordinate2D coordinates[] = {
+        {-122.366278,37.818844},
+        {-122.365248,37.819267},
+        {-122.365640,37.819861},
+        {-122.366669,37.819429},
+        {-122.366278,37.818844}
+    };
+    
+    MKPolygon* polygon = [MKPolygon polygonWithCoordinates:coordinates count:sizeof(coordinates)];
+    
+    MKMapRect boundingMapRect = polygon.boundingMapRect;
+    
+    assertThatDouble(1.5,equalToDouble(boundingMapRect.origin.x));
+    
+    
 }
 @end
